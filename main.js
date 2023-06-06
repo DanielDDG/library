ba89a4d9cec02d01a5d8fec49de96f01ef2c2284
DanielDDG <DanielGonzalezDDG@outlook.com>
2023-06-06 11:13:56 -0400

    Complete styling

--- main.js
+++ main.js
@@ -1,20 +1,27 @@
 let myLibrary = [];
+let planningCount = 0;
+let readingCount = 1;
+let completedCount = 0;
 
 const add = document.getElementById('add');
 const modal = document.getElementById('form');
 const books = document.getElementById('books');
 const close = document.getElementById('close');
+const remove = document.getElementById('remove');
+const readCount = document.getElementById('readCount');
+const planCount = document.getElementById('planCount');
+const completeCount = document.getElementById('completeCount');
 
 function retrieveForm() {
     const title = document.getElementById('title').value;
     const author = document.getElementById('author').value;
     const pages = document.getElementById('pages').value;
     const year = document.getElementById('year').value;
-    const status = document.getElementById('status').value;
 
     const entry = new Book(title, author, pages, year, status);
     myLibrary.push(entry);
-    addBook();
+
+    addBook.call(entry);
 }
 
 function Book(title, author, pages, year, status) {
@@ -27,46 +34,94 @@ function Book(title, author, pages, year, status) {
 
 function allBooks() {
     for (i = 0; i < myLibrary.length; i++) {
-        addBook();
+        addBook.call(myLibrary[i]);
     }
 }
 
 function addBook() {
     let newBook = document.createElement('div');
+    let closeArrow = document.createElement('span');
+    let container = document.createElement('div');
     let newTitle = document.createElement('p');
     let newAuthor = document.createElement('p');
     let newPages = document.createElement('p');
     let newYear = document.createElement('p');
-    let newStatus = document.createElement('p');
+    let newStatus = document.createElement('select');
+    let reading = document.createElement('option');
+    let planning = document.createElement('option');
+    let completed = document.createElement('option');
 
-    newTitle.textContent = myLibrary[i].title;
-    newAuthor.textContent = myLibrary[i].author;
-    newPages.textContent = myLibrary[i].pages;
-    newYear.textContent = myLibrary[i].year;
-    newStatus.textContent = myLibrary[i].status;
+    readCount.textContent = 'Reading: ' + readingCount;
+    planCount.textContent = 'Planning: ' + planningCount;
+    completeCount.textContent = 'Completed: ' + completedCount;
+    closeArrow.textContent = '\u00D7';
+    newTitle.textContent = this.title;
+    newAuthor.textContent = this.author;
+    newPages.textContent = this.pages + ' Pages';
+    newYear.textContent = this.year;
+    newStatus.textContent = this.status;
+    reading.textContent = 'Reading';
+    planning.textContent = 'Planning';
+    completed.textContent = 'Completed';
+
+    closeArrow.setAttribute('data-booknumber', [i]);
+    newBook.setAttribute('data-booknumber', [i]);
+    let arrowNumber = closeArrow.getAttribute('data-booknumber');
+    let bookNumber = closeArrow.getAttribute('data-booknumber');
+    
+    newBook.className = 'entry';
+    closeArrow.id = 'remove';
+
+    closeArrow.addEventListener('click', () => {
+        if (arrowNumber === bookNumber) {
+            newBook.remove();
+        }
+    });
+
+    newStatus.addEventListener('change', function() {
+        const option = this.value;
+
+        if (option === 'Planning') {
+            readingCount--;
+            planningCount++;
+            newBook.style.backgroundColor = 'rgb(237, 240, 74)';
+        } else if (option === 'Completed') {
+            readingCount--;
+            completedCount++;
+            newBook.style.backgroundColor = 'rgb(90, 204, 224)';
+        } else {
+            readingCount++;
+            newBook.style.backgroundColor = 'rgb(84, 226, 115)';
+        }
+    });
+
+    console.log(readingCount);
+    console.log(planningCount);
+    console.log(completedCount);
 
     books.appendChild(newBook);
-    newBook.appendChild(newTitle);
-    newBook.appendChild(newAuthor);
-    newBook.appendChild(newPages);
-    newBook.appendChild(newYear);
+    newBook.appendChild(closeArrow);
+    newBook.appendChild(container);
+    container.appendChild(newTitle);
+    container.appendChild(newAuthor);
+    container.appendChild(newPages);
+    container.appendChild(newYear);
     newBook.appendChild(newStatus);
+
+    newStatus.appendChild(reading);
+    newStatus.appendChild(planning);
+    newStatus.appendChild(completed);
 
     document.getElementById('title').value = '';
     document.getElementById('author').value = '';
     document.getElementById('pages').value = '';
     document.getElementById('year').value = '';
-    document.getElementById('status').value = '';
-
-    console.log(myLibrary);
 }
-
-
 
 modal.addEventListener('submit', (event) => {
     event.preventDefault();
     retrieveForm();
-    modal.reset();
+    readingCount++;
 });
 
 add.addEventListener('click', () => {
@@ -77,7 +132,4 @@ close.addEventListener('click', () => {
     modal.style.display = "none";
 });
 
-const book1 = new Book('Mistborn', 'Brandon Sanderson', '294', '2008', 'Completed');
-const book2 = new Book('The Hobbit', 'J.R.R. Tolkien', '398', '1964', 'Reading');
-myLibrary.push(book1, book2);
 allBooks();
